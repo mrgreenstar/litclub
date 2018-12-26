f9b1706e20e069620b3d7a70978e606f8fa1ba7b
mrgreenstar <andrey.lalaev@gmail.com>
2018-12-26 19:33:18 +0300

    added awful json parse

--- src/main/java/com/mrgreenstar/litclub/IndexController.java
+++ src/main/java/com/mrgreenstar/litclub/IndexController.java
@@ -1,15 +1,30 @@
 package com.mrgreenstar.litclub;
 
+import com.mrgreenstar.litclub.Entity.User;
+import com.mrgreenstar.litclub.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(path="/")
 public class IndexController {
-    @GetMapping("/")
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping(path="/")
     public ModelAndView index(ModelAndView model) {
+        model.setViewName("redirect:/1");
+        return model;
+    }
+
+    @GetMapping(path="/{id}")
+    public ModelAndView certainIndex(@PathVariable Long id,
+                                     ModelAndView model) {
         model.setViewName("index");
         return model;
     }
--- src/main/resources/static/app/app.jsx
+++ src/main/resources/static/app/app.jsx
@@ -2,14 +2,29 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {info: []};
+    }
+
+    componentDidMount() {
+        const parts = window.location.href.split('/');
+        const curId = parts[parts.length - 1];
+        fetch('http://localhost:8080/api/users/' + curId)
+            .then(response => response.json())
+            .then(response => this.setState({info: response}));
+    }
+
     render() {
         return(
             <div>
-            <p>
-                Hello!
-            </p>
+                {Object.keys(this.state.info).map(i => (
+                    <div key={i}>
+                        {this.state.info[i].toString()}
+                    </div>
+                ))}
             </div>
-        )
+        );
     }
 }
 
