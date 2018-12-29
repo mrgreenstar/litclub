f43c301de9b5073122598317f185e0ffc02f6df5
mrgreenstar <andrey.lalaev@gmail.com>
2018-12-29 17:15:13 +0300

    changed return in App component

--- frontend/src/js/addUser.js
+++ frontend/src/js/addUser.js
@@ -46,48 +46,54 @@ class AddUser extends React.Component {
     }
 
     render() {
+        const form =
+            <Form onSubmit={this.handleSubmit} method="POST">
+            <FormGroup row>
+                <Label sm={{size:2, offset: 1}}>First name</Label>
+                <Col sm={8}>
+                    <Input type="text" name="firstName" placeholder="Your first name" onChange={this.handleChange}/>
+                </Col>
+            </FormGroup>
+            <FormGroup row>
+                <Label sm={{size:2, offset: 1}}>Second name</Label>
+                <Col sm={8}>
+                    <Input type="text" name="lastName" placeholder="Your last name" onChange={this.handleChange}/>
+                </Col>
+            </FormGroup>
+            <FormGroup row>
+                <Label sm={{size:2, offset: 1}}>Email</Label>
+                <Col sm={8}>
+                    <Input type="email" name="email" placeholder="Type your email here" onChange={this.handleChange}/>
+                </Col>
+            </FormGroup>
+            <FormGroup row>
+                <Label sm={{size:2, offset: 1}}>Login</Label>
+                <Col sm={8}>
+                    <Input type="text" name="login" placeholder="Type your login here" onChange={this.handleChange}/>
+                </Col>
+            </FormGroup>
+            <FormGroup row>
+                <Label sm={{size:2, offset: 1}}>Password</Label>
+                <Col sm={8}>
+                    <Input type="text" name="password" placeholder="Type your password here" onChange={this.handleChange}/>
+                </Col>
+            </FormGroup>
+            <FormGroup check row>
+                <Col sm={{size: 6, offset: 5}}>
+                    <Button type="submit" color="success">Add new user</Button>
+                </Col>
+            </FormGroup>
+        </Form>;
+
         if (this.state.isUserCreated) {
-            return <Message/>
+            return (
+                <React.Fragment>
+                    <Message/>
+                    {form}
+                </React.Fragment>
+            )
         }
-        return (
-                <Form onSubmit={this.handleSubmit} method="POST">
-                    <FormGroup row>
-                        <Label sm={{size:2, offset: 1}}>First name</Label>
-                        <Col sm={8}>
-                            <Input type="text" name="firstName" placeholder="Your first name" onChange={this.handleChange}/>
-                        </Col>
-                    </FormGroup>
-                    <FormGroup row>
-                        <Label sm={{size:2, offset: 1}}>Second name</Label>
-                        <Col sm={8}>
-                            <Input type="text" name="lastName" placeholder="Your last name" onChange={this.handleChange}/>
-                        </Col>
-                    </FormGroup>
-                    <FormGroup row>
-                        <Label sm={{size:2, offset: 1}}>Email</Label>
-                        <Col sm={8}>
-                            <Input type="email" name="email" placeholder="Type your email here" onChange={this.handleChange}/>
-                        </Col>
-                    </FormGroup>
-                    <FormGroup row>
-                        <Label sm={{size:2, offset: 1}}>Login</Label>
-                        <Col sm={8}>
-                            <Input type="text" name="login" placeholder="Type your login here" onChange={this.handleChange}/>
-                        </Col>
-                    </FormGroup>
-                    <FormGroup row>
-                        <Label sm={{size:2, offset: 1}}>Password</Label>
-                        <Col sm={8}>
-                            <Input type="text" name="password" placeholder="Type your password here" onChange={this.handleChange}/>
-                        </Col>
-                    </FormGroup>
-                    <FormGroup check row>
-                        <Col sm={{size: 6, offset: 5}}>
-                            <Button type="submit" color="success">Add new user</Button>
-                        </Col>
-                    </FormGroup>
-                </Form>
-        )
+        return form;
     }
 }
 
